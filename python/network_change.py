9c1fb976695d7964f79a519bdcf35bf25a1be4d6
Juan Perez <juancamiloperez2009@hotmail.com>
2022-03-30 11:10:30 -0500

    Se crea script python para conectar a una red de comunicaciones especifica

---
+++ python/network_change.py
@@ -0,0 +1,17 @@
+import subprocess
+redconexion="CONEXION///PEREZ..." # cambiar por la red correcta a conectar
+print('Ingresa el SSID de la red a verificar:')
+red= input()
+inicio=312
+fin=(inicio+len(red))
+#print(fin)
+p=subprocess.run("netsh wlan show interfaces", stdout=subprocess.PIPE, shell=True)
+texto=p.stdout.decode('unicode_escape')
+#print(texto)
+subtexto=texto[inicio:fin]
+#print(subtexto)
+if red == subtexto:
+  p2=subprocess.run('netsh wlan connect name='+'"'+redconexion+'"', stdout=subprocess.PIPE, shell=True)
+  print(p2.stdout.decode('unicode_escape'))
+else:
+  print("La red ingresada no corresponde a la red conectada , No se hizo nada!")
